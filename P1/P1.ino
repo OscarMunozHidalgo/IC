428332214cee33dc2c7e7ef9da02414d7944bfc1
Pablo <zerytv75@gmail.com>
2024-10-02 15:18:18 +0100

    Parte 3 completada

--- P1/P1.ino
+++ P1/P1.ino
@@ -1,23 +1,51 @@
 #include <time.h>
 #include <RTCZero.h>
+#include <Arduino_MKRMEM.h>
 
 // rtc object
 RTCZero rtc;
 
-// OJO al carácter volátil de estas variables
 volatile uint32_t _period_sec = 0;
 volatile uint16_t _rtcFlag = 0;
 
-// Macro útil para medir el tiempo transcurrido en milisegundos
+Arduino_W25Q16DV flash(SPI1, FLASH_CS);
+char filename[] = "datos.txt";
+
+// Macro para medir el tiempo transcurrido en milisegundos
 #define elapsedMilliseconds(since_ms) (uint32_t)(millis() - since_ms)
 
 void setup() 
 {
+  pinMode(LORA_RESET, OUTPUT);
+  digitalWrite(LORA_RESET, LOW); 
+
   // Registramos el tiempo de comienzo
   uint32_t t_start_ms = millis();
 
   SerialUSB.begin(9600);
   while(!SerialUSB) {;}
+
+  flash.begin();
+
+  // Montamos el sistema de archivos
+  SerialUSB.println("Mounting ...");
+  int res = filesystem.mount();
+  if(res != SPIFFS_OK && res != SPIFFS_ERR_NOT_A_FS) {
+    SerialUSB.println("mount() failed with error code "); 
+    SerialUSB.println(res); 
+    exit(EXIT_FAILURE);
+  }
+
+  // Creamos un nuevo fichero
+  // Podríamos usar create(), pero open() proporciona más flexibilidad (flags)
+  File file = filesystem.open(filename,  CREATE | TRUNCATE);
+  if (!file) {
+    SerialUSB.print("Creation of file ");
+    SerialUSB.print(filename);
+    SerialUSB.print(" failed. Aborting ...");
+    on_exit_with_error_do();
+  }
+  file.close();
 
   SerialUSB.print(__DATE__);
   SerialUSB.print(" ");
@@ -49,6 +77,30 @@ void loop()
       
     // Imprimimos la fecha y la hora
     printDateTime();
+    delay(1000);
+
+        // Abrimos el fichero para lectura
+    File file = filesystem.open(filename,  READ_ONLY);
+    if (!file) {
+      SerialUSB.print("Opening file ");
+      SerialUSB.print(filename);
+      SerialUSB.print(" failed for reading. Aborting ...");
+      on_exit_with_error_do();
+    }    
+    SerialUSB.print("Reading file contents:\n");
+    
+    // Leemos el contenido del fichero hasta alcanzar la marca EOF
+    while(!file.eof()) {
+      char c;
+      int const bytes_read = file.read(&c, sizeof(c));
+      if (bytes_read) {
+        SerialUSB.print(c);
+        if (c == '\n') SerialUSB.print("\t ");
+      }
+    }
+
+    // Cerramos el fichero
+    file.close();
 
     // Decrementamos _rtcFlag
     _rtcFlag--;
@@ -97,12 +149,13 @@ void printDateTime()
   
   // Generamos e imprimimos la fecha y la hora
   char dateTime[32]; 
-  snprintf(dateTime, sizeof(dateTime),"%s %4u/%02u/%02u %02u:%02u:%02u",
+  snprintf(dateTime, sizeof(dateTime),"%s %4u/%02u/%02u %02u:%02u:%02u\n",
            weekDay[stm.tm_wday], 
            stm.tm_year + 1900, stm.tm_mon + 1, stm.tm_mday, 
            stm.tm_hour, stm.tm_min, stm.tm_sec);
 
-  SerialUSB.println(dateTime);
+  //SerialUSB.println(dateTime);
+  writeInFile(dateTime);
 }
 
 // --------------------------------------------------------------------------------
@@ -133,4 +186,32 @@ void alarmCallback()
   
   // Reprogramamos la alarma usando el mismo periodo 
   rtc.setAlarmEpoch(rtc.getEpoch() + _period_sec);
+}
+
+void writeInFile(const char* dateTime)
+{
+    File file = filesystem.open(filename,  WRITE_ONLY | APPEND);
+    if (!file) {
+      SerialUSB.print("Opening file ");
+      SerialUSB.print(filename);
+      SerialUSB.print(" failed for appending. Aborting ...");
+      on_exit_with_error_do();
+    }
+    
+    int const bytes_to_write = strlen(dateTime);
+    int const bytes_written = file.write((void *)dateTime, bytes_to_write);
+    if (bytes_to_write != bytes_written) {
+      SerialUSB.print("write() failed with error code "); 
+      SerialUSB.println(filesystem.err());
+      SerialUSB.println("Aborting ...");
+      on_exit_with_error_do();
+    }
+    // Cerramos el fichero
+    file.close();
+}
+
+void on_exit_with_error_do()
+{
+  filesystem.unmount();
+  exit(EXIT_FAILURE);
 }
